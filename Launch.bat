cdbbee484628949415c4956402abe775ade44f47
7teen <71920969+17teen@users.noreply.github.com>
2021-06-19 22:54:53 +0100

    Beta Update

--- Launch.bat
+++ Launch.bat
@@ -1,9 +1,13 @@
 @echo off
 title Launching Modules v.1.1.0
-:top
 cls
-node launch.js
+echo [!] Modules will be installed on your system...
 pause
+cls
+echo Tip: For the first installation, you are only required to press the Enter key untill you have nothing else to fill in.
+pause
+cls
+echo Starting:
     call npm init
     call npm install discord.js@12.5.1
     call npm install chalk
@@ -11,4 +15,3 @@ pause
     call npm install mv
 pause
 exit
-goto :top
